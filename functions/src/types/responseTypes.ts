257b895dd98b8deaae79196d011571104d6fea62
andreirawan97 <andreirawan97@gmail.com>
2020-05-15 12:18:53 +0700

    Add firebase endpoint for first aid

--- functions/src/types/responseTypes.ts
+++ functions/src/types/responseTypes.ts
@@ -8,9 +8,24 @@ export type UserInfo = {
     phone_number: string;
 } | EmptyObject;
 
+type FirstAidDetailQNA = {
+    title: string;
+    detail: string;
+}
+
 export type QueryLogin = {
     success: boolean,
     message: string,
     unique_id: string,
     user_info: UserInfo,
 };
+
+export type QueryFirstAid = Array<string>;
+
+export type QueryFirstAidDetail =   {
+    name: string,
+    header_img: string, // "image_allergy.png"
+    icon_img: string, // "icon_allergy.png"
+    emergency_handling: Array<string>,
+    qna: Array<FirstAidDetailQNA>
+  } | EmptyObject;
